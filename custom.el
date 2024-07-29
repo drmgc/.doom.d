140694acfd2918a0cfd3df0dfe599ba9186e1696
Sergey Naumov <drmgc@yandex.ru>
2024-07-29 15:53:31 +0500

    `safe-local-variable-values`: `ispell-dictionary` `ru`

--- custom.el
+++ custom.el
@@ -5,7 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("9f297216c88ca3f47e5f10f8bd884ab24ac5bc9d884f0f23589b0a46a608fe14" default))
- '(package-selected-packages '(blamer modus-themes mindre-theme apropospriate-theme)))
+ '(package-selected-packages '(blamer modus-themes mindre-theme apropospriate-theme))
+ '(safe-local-variable-values '((ispell-dictionary . "ru"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
