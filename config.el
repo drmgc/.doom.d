eb32ec6d5dcd91696ec4b015c0fa58126b9b008a
Sergey Naumov <drmgc@yandex.ru>
2025-01-14 18:01:58 +0500

    configure gdscript

--- config.el
+++ config.el
@@ -331,3 +331,25 @@
     '(transient-append-suffix 'magit-diff '(-1 -1)
        [("D" "Difftastic diff (dwim)" difftastic-magit-diff)
         ("S" "Difftastic show" difftastic-magit-show)])))
+
+
+;;
+;;; gdscript
+
+(defun lsp--gdscript-ignore-errors (original-function &rest args)
+  "Ignore the error message resulting from Godot not replying to the `JSONRPC' request."
+  (if (string-equal major-mode "gdscript-mode")
+      (let ((json-data (nth 0 args)))
+        (if (and (string= (gethash "jsonrpc" json-data "") "2.0")
+                 (not (gethash "id" json-data nil))
+                 (not (gethash "method" json-data nil)))
+            nil ; (message "Method not found")
+          (apply original-function args)))
+    (apply original-function args)))
+;; Runs the function `lsp--gdscript-ignore-errors` around `lsp--get-message-type` to suppress unknown notification errors.
+(advice-add #'lsp--get-message-type :around #'lsp--gdscript-ignore-errors)
+
+(use-package gdscript-mode
+  :custom
+  (gdscript-use-tab-indents t)
+  (gdscript-indent-offset 2))
